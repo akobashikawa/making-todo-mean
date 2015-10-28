7b46500c557f286b311d483d1ce2f122a93d6b92
Rulo Kobashikawa <akobashikawa@gmail.com>
2015-10-28 16:06:14 -0500

    Implementa lista de items

--- html/js/app.js
+++ html/js/app.js
@@ -9,6 +9,13 @@
     function TodoController($scope) {
         var todo = this;
 
+        $scope.items = [
+            {id: 1, text: 'Item 1', order: 1},
+            {id: 2, text: 'Item 2', order: 2},
+            {id: 3, text: 'Item 3', order: 3},
+            {id: 4, text: 'Item 4', order: 4},
+            {id: 5, text: 'Item 5', order: 5}
+        ];
     }
 
 })();
