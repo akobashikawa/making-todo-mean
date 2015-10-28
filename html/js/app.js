dfbb06700811078c3136f5a390dd9071d020cb13
Rulo Kobashikawa <akobashikawa@gmail.com>
2015-10-28 16:53:16 -0500

    Actualiza comportamiento de add

--- html/js/app.js
+++ html/js/app.js
@@ -17,8 +17,24 @@
             {id: 5, text: 'Item 5'}
         ];
 
+        todo.nextId = function () {
+            var items = $scope.items;
+            var max = 0;
+            for (var i = 0; i < items.length; i++) {
+                if (items[i].id > max) {
+                    max = items[i].id;
+                }
+            }
+            return max + 1;
+        };
+
         $scope.add = function() {
-            //$scope.items.push();
+            var id = todo.nextId();
+            var text = $scope.newItem;
+            $scope.items.push({
+                id: id,
+                text: text
+            });
         };
     }
 
