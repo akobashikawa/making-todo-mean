a7caaf6cddfe3fec59bfee2c3402017d1d4209a9
Rulo Kobashikawa <akobashikawa@gmail.com>
2015-10-28 16:51:25 -0500

    Cambia lista de objetos simples por lista de objetos

--- html/js/app.js
+++ html/js/app.js
@@ -10,15 +10,15 @@
         var todo = this;
 
         $scope.items = [
-            'Item 1',
-            'Item 2',
-            'Item 3',
-            'Item 4',
-            'Item 5'
+            {id: 1, text: 'Item 1'},
+            {id: 2, text: 'Item 2'},
+            {id: 3, text: 'Item 3'},
+            {id: 4, text: 'Item 4'},
+            {id: 5, text: 'Item 5'}
         ];
 
         $scope.add = function() {
-            $scope.items.push($scope.newItem);
+            //$scope.items.push();
         };
     }
 
